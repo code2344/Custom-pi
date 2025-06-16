fb602878e997d97f6a3e36bda018631cd5f6e566
code2344 <71059013+code2344@users.noreply.github.com>
2025-06-16 13:29:05 +1000

    Update build-image.sh

--- custom-pi-boot 2/build-image.sh
+++ custom-pi-boot 2/build-image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 IMG="$1"
 LOOP=$(sudo losetup --show -fP "$IMG")
 
@@ -10,10 +11,10 @@ MNT_ROOT=$(mktemp -d)
 sudo mount "${LOOP}p1" "$MNT_BOOT"
 sudo mount "${LOOP}p2" "$MNT_ROOT"
 
-# Copy custom files
-sudo cp -r config/boot/* "$MNT_BOOT/"
-sudo cp -r config/etc/* "$MNT_ROOT/etc/"
-sudo cp -r config/home/pi/* "$MNT_ROOT/home/pi/"
+# Copy custom files from script directory
+sudo cp -r "$SCRIPT_DIR/config/boot/"* "$MNT_BOOT/"
+sudo cp -r "$SCRIPT_DIR/config/etc/"* "$MNT_ROOT/etc/"
+sudo cp -r "$SCRIPT_DIR/config/home/pi/"* "$MNT_ROOT/home/pi/"
 sudo chmod +x "$MNT_ROOT/etc/rc.local"
 sudo chmod +x "$MNT_ROOT/home/pi/ebike_demo"
 
