f03034ad4570c4139fe0aae91181f5e2e3d169c3
code2344 <71059013+code2344@users.noreply.github.com>
2025-06-16 13:03:36 +1000

    Add files via upload

---
+++ custom-pi-boot 2/README.md
@@ -0,0 +1,11 @@
+# Custom Raspberry Pi OS Image
+
+This repository builds a Raspberry Pi OS Lite image that:
+- Uses a custom boot splash screen
+- Launches a demo binary on startup
+- Does not start the desktop environment
+
+## Setup
+1. Push your edits to main
+2. Wait for the GitHub Action to complete
+3. Download the built `.img` file from the Actions tab
---
+++ custom-pi-boot 2/build-image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+IMG="$1"
+LOOP=$(sudo losetup --show -fP "$IMG")
+
+MNT_BOOT=$(mktemp -d)
+MNT_ROOT=$(mktemp -d)
+
+sudo mount "${LOOP}p1" "$MNT_BOOT"
+sudo mount "${LOOP}p2" "$MNT_ROOT"
+
+# Copy custom files
+sudo cp -r config/boot/* "$MNT_BOOT/"
+sudo cp -r config/etc/* "$MNT_ROOT/etc/"
+sudo cp -r config/home/pi/* "$MNT_ROOT/home/pi/"
+sudo chmod +x "$MNT_ROOT/etc/rc.local"
+sudo chmod +x "$MNT_ROOT/home/pi/ebike_demo"
+
+sudo umount "$MNT_BOOT"
+sudo umount "$MNT_ROOT"
+sudo losetup -d "$LOOP"
